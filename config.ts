89d496ce521a4d1d4e9371095a162413650dbfe4
kyle <lloydpearce4@gmail.com>
2024-05-09 15:53:00 -0700

    updating of technology & aboutus page

---
+++ config.ts
@@ -0,0 +1,15 @@
+type Partner = {
+	partnerName: string;
+	imgUrl: string;
+};
+
+const PARTNERS: Partner[] = [
+	{ partnerName: "Partner 1", imgUrl: "/amr.png" },
+	{ partnerName: "Partner 2", imgUrl: "/amr.png" },
+	{ partnerName: "Partner 3", imgUrl: "/amr.png" },
+	{ partnerName: "Partner 4", imgUrl: "/amr.png" },
+	{ partnerName: "Partner 5", imgUrl: "/amr.png" },
+	{ partnerName: "Partner 6", imgUrl: "/amr.png" },
+];
+
+export default PARTNERS;

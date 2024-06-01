1b4163c7d11ed7e93348240b0cf41ad1ff7d64fe
Lloyd <lloydpearce4@gmail.com>
2024-06-01 16:06:24 -0700

    updating agv table:

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,4 @@
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, Check, X } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
@@ -83,6 +83,15 @@ export default function ProductsHome() {
                         <TableHead>Max Lifting Height</TableHead>
                         <TableHead>Max Payload Weight</TableHead>
                         <TableHead>Aisle Width</TableHead>
+                        <TableHead>Pallet Type 1</TableHead>
+                        <TableHead>Pallet Type 2</TableHead>
+                        <TableHead>Pallet Type 3</TableHead>
+                        <TableHead>Pallet Type 4</TableHead>
+                        <TableHead>Pallet Type 5</TableHead>
+                        <TableHead>Horizontal Sorting</TableHead>
+                        <TableHead>Ground Storage</TableHead>
+                        <TableHead>Racking Storage</TableHead>
+                        <TableHead>High Racking Storage</TableHead>
                         {/* <TableHead className="text-right">Amount</TableHead> */}
                     </TableRow>
                 </TableHeader>
@@ -90,17 +99,52 @@ export default function ProductsHome() {
                     {agvTableData.map((robot) => (
                         <TableRow key={robot.id}>
                             <TableCell className="font-semibold">
-                                <Link href={`/products/${robot.id}`}>
-                                    {robot.name}
-                                </Link>
+                                {robot.name}
                             </TableCell>
                             <TableCell>{robot.liftHeight}</TableCell>
                             <TableCell>{robot.liftWeight}</TableCell>
                             <TableCell>{robot.aisleWidth}</TableCell>
+                            <BooleanTableCell value={robot.palletType1} />
+                            <BooleanTableCell value={robot.palletType2} />
+                            <BooleanTableCell value={robot.palletType3} />
+                            <BooleanTableCell value={robot.palletType4} />
+                            <BooleanTableCell value={robot.palletType5} />
+                            <BooleanTableCell value={robot.horizontalSorting} />
+                            <BooleanTableCell value={robot.groundStorage} />
+                            <BooleanTableCell value={robot.rackingStorage} />
+                            <BooleanTableCell
+                                value={robot.highRackingStorage}
+                            />
+                            <TableCell>
+                                <Link href={`/products/${robot.id}`}>
+                                    <Button variant="outline" size="icon">
+                                        <ChevronRight className="h-4 w-4" />
+                                    </Button>
+                                </Link>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
         </main>
     );
+}
+
+// Renders a table cell if the value is a boolean value
+function BooleanTableCell({ value }: { value: boolean | null }) {
+    if (value !== null && value) {
+        return (
+            <TableCell>
+                <Check />
+            </TableCell>
+        );
+    } else if (value !== null && !value) {
+        return (
+            <TableCell className="text-center">
+                <X />
+            </TableCell>
+        );
+    } else {
+        return <TableCell>N/A</TableCell>;
+    }
 }

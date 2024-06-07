def973447f5643079d0d12b6664e9d782cbb80d7
Kyle Hu <lloydpearce4@gmail.com>
2024-06-06 19:29:50 -0700

    Updates

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import NavbarLink from "./NavbarLink";
 
 export default function DashboardNavbar() {
     return (
-        <nav className="w-ful sticky top-0 z-50 border-b-zinc-300 bg-white py-4 shadow-xl xl:px-14 2xl:px-28">
+        <nav className="w-ful sticky top-0 z-50 border-b-zinc-300 bg-white py-4 shadow-md xl:px-14 2xl:px-28">
             <div className="flex justify-between">
                 <Image
                     src="/logo.png"
@@ -22,8 +22,8 @@ export default function DashboardNavbar() {
 
                 <div className="flex items-center gap-6 xl:gap-8">
                     <NavbarLink link="/" text="Home" />
-                    <NavbarLink link="/products" text="Products" />
                     <NavbarLink link="/technology" text="Technology" />
+                    <NavbarLink link="/products" text="Products" />
                     <NavbarLink
                         link="/manufacturing"
                         text="Manufacturing Capability"
@@ -31,9 +31,11 @@ export default function DashboardNavbar() {
                     <NavbarLink link="/service" text="Service" />
                     <NavbarLink link="/news" text="News" />
                     <NavbarLink link="/about" text="About Us" />
-                    <button className="= rounded-xl bg-orange-400 p-2 font-semibold text-white transition duration-300 ease-in-out 2xl:text-[1.1rem]">
-                        Contact Us
-                    </button>
+                    <Link href="/contact">
+                        <button className="= rounded-xl bg-orange-400 p-2 font-semibold text-white transition duration-300 ease-in-out 2xl:text-[1.1rem]">
+                            Contact Us
+                        </button>
+                    </Link>
                 </div>
             </div>
         </nav>

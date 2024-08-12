65443a3be0e74ca92e14917249cc6e0aab02694a
Kyle Hu <lloydpearce4@gmail.com>
2024-08-12 12:13:59 -0700

    Contact form now uses a .env variable for the access key

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,38 +1,3 @@
-// import { Button } from "../ui/button"
-// import FormInput from "./FormInput"
-
-// export default function ContactForm() {
-//     return (
-// <form className="flex flex-col items-center gap-7 rounded-lg border border-zinc-400 p-10 shadow-2xl 3xl:w-[50rem]">
-//     <div className="flex w-full gap-5">
-//         <FormInput labelText="First Name" placeholder="Bill" />
-//         <FormInput labelText="Last Name" placeholder="Smith" />
-//     </div>
-
-//     <div className="flex w-full gap-5">
-//         <FormInput
-//             labelText="Email"
-//             placeholder="billsmith@email.com"
-//         />
-//         <FormInput
-//             labelText="Phone Number"
-//             placeholder="012-345-6789"
-//         />
-//     </div>
-
-//     <div className="flex w-full flex-col gap-5">
-//         <label className="3xl:text-xl">Message</label>
-//         <textarea
-//             className="w-full rounded-md border border-zinc-600 p-3"
-//             placeholder="Your message"
-//         ></textarea>
-//     </div>
-
-//     <Button className="text-lg">Send</Button>
-// </form>
-//     )
-// }
-
 "use client"
 
 import { useState } from "react"
@@ -45,10 +10,13 @@ export default function Contact() {
 
     const onSubmit = async (event: any) => {
         event.preventDefault()
-        setResult("Sending....")
+        setResult("Sending Your Message...")
         const formData = new FormData(event.target)
 
-        formData.append("access_key", "c18b7497-dd33-4b95-aa23-0f9f3b67fddc")
+        formData.append(
+            "access_key",
+            process.env.NEXT_PUBLIC_FORM_ACCESS_KEY || "",
+        )
 
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",

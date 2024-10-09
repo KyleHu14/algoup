e8b6a67c1599ab5ae1ca49ca126336bb4470f5b2
Kyle Hu <lloydpearce4@gmail.com>
2024-10-09 13:50:43 -0700

    testing service image

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image"
 
 export default function Service() {
     return (
-        <main className="flex h-screen flex-col items-center py-14 2xl:py-20">
+        <main className="flex flex-col items-center justify-center py-20">
             <h1 className="text-4xl font-bold">Our Service Process</h1>
-            <div className="relative flex h-[35rem] w-[50rem] 2xl:h-[35rem] 2xl:w-[70rem]">
+            <div className="relative h-[15rem] w-[30rem] xl:h-[35rem] xl:w-[50rem] 2xl:h-[35rem] 2xl:w-[70rem]">
                 <Image
                     src="/services/service-chart.png"
                     alt="Service Chart"

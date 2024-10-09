8b1a69ea095ebad01427be2192c95d08680e6c41
Kyle Hu <lloydpearce4@gmail.com>
2024-10-09 13:57:24 -0700

    adjusted service page

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image"
 export default function Service() {
     return (
         <main className="flex flex-col items-center justify-center py-20">
-            <h1 className="text-2xl font-bold lg:text-4xl">
+            <h1 className="text-xl font-bold md:text-2xl lg:text-4xl">
                 Our Service Process
             </h1>
-            <div className="relative h-[12rem] w-[24rem] md:h-[15rem] md:w-[30rem] xl:h-[35rem] xl:w-[50rem] 2xl:h-[35rem] 2xl:w-[70rem]">
+            <div className="relative h-[12rem] w-[24rem] sm:h-[15rem] sm:w-[30rem] md:h-[20rem] md:w-[40rem] lg:h-[25rem] lg:w-[50rem] xl:h-[35rem] xl:w-[70rem] 2xl:h-[35rem] 2xl:w-[70rem]">
                 <Image
                     src="/services/service-chart.png"
                     alt="Service Chart"

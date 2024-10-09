e5b1274024bae051e4eca7f50f410721e35916bd
Kyle Hu <lloydpearce4@gmail.com>
2024-10-09 13:53:09 -0700

    image push

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -3,8 +3,10 @@ import Image from "next/image"
 export default function Service() {
     return (
         <main className="flex flex-col items-center justify-center py-20">
-            <h1 className="text-4xl font-bold">Our Service Process</h1>
-            <div className="relative h-[15rem] w-[30rem] xl:h-[35rem] xl:w-[50rem] 2xl:h-[35rem] 2xl:w-[70rem]">
+            <h1 className="text-2xl font-bold lg:text-4xl">
+                Our Service Process
+            </h1>
+            <div className="relative h-[13rem] w-[26rem] md:h-[15rem] md:w-[30rem] xl:h-[35rem] xl:w-[50rem] 2xl:h-[35rem] 2xl:w-[70rem]">
                 <Image
                     src="/services/service-chart.png"
                     alt="Service Chart"

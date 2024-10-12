8bd369a1e16a4697fbbc9186c88747010b5362d1
Kyle Hu <lloydpearce4@gmail.com>
2024-10-11 20:48:10 -0700

    updated contact page to be responsive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,13 +4,15 @@ export default function ContactPage() {
     return (
         <main className="py-20">
             <header className="text-center">
-                <h1 className="font-semibold 3xl:text-4xl">Contact Algoup</h1>
-                <p className="mt-5 3xl:text-xl">
+                <h1 className="text-2xl font-semibold sm:text-3xl lg:text-4xl">
+                    Contact Algoup
+                </h1>
+                <p className="text-md mx-16 mt-2 md:text-lg lg:text-xl 3xl:text-xl">
                     Whether its talking an expert, asking an inquiry, or giving
                     feedback, we're all ears!
                 </p>
             </header>
-            <section className="mt-10 flex justify-center">
+            <section className="mt-4 flex justify-center">
                 <ContactForm />
             </section>
         </main>
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -37,47 +37,45 @@ export default function Contact() {
     }
 
     return (
-        <div>
-            <form
-                className="flex flex-col items-center gap-7 rounded-lg border border-zinc-400 p-10 shadow-2xl 3xl:w-[50rem]"
-                onSubmit={onSubmit}
-            >
-                <div className="flex w-full gap-5">
-                    <FormInput
-                        name="name"
-                        labelText="Name"
-                        placeholder="Bill Smith"
-                    />
-                </div>
+        <form
+            className="flex w-[60%] flex-col items-center gap-7 rounded-lg border border-zinc-400 p-4 shadow-2xl lg:p-10"
+            onSubmit={onSubmit}
+        >
+            <div className="flex w-full gap-5">
+                <FormInput
+                    name="name"
+                    labelText="Name"
+                    placeholder="Bill Smith"
+                />
+            </div>
 
-                <div className="flex w-full gap-5">
-                    <FormInput
-                        name="email"
-                        labelText="Email"
-                        placeholder="billsmith@email.com"
-                    />
-                    <FormInput
-                        name="phone"
-                        labelText="Phone Number"
-                        placeholder="012-345-6789"
-                    />
-                </div>
+            <div className="flex w-full flex-col gap-5 lg:flex-row">
+                <FormInput
+                    name="email"
+                    labelText="Email"
+                    placeholder="billsmith@email.com"
+                />
+                <FormInput
+                    name="phone"
+                    labelText="Phone Number"
+                    placeholder="012-345-6789"
+                />
+            </div>
 
-                <div className="flex w-full flex-col gap-5">
-                    <label className="3xl:text-xl">Message</label>
-                    <textarea
-                        name="message"
-                        className="w-full rounded-md border border-zinc-600 p-3"
-                        placeholder="Your message"
-                        required
-                    ></textarea>
-                </div>
+            <div className="flex w-full flex-col gap-5">
+                <label className="3xl:text-xl">Message</label>
+                <textarea
+                    name="message"
+                    className="w-full rounded-md border border-zinc-600 p-3"
+                    placeholder="Your message"
+                    required
+                ></textarea>
+            </div>
 
-                <Button type="submit" className="text-lg">
-                    Send
-                </Button>
-                <span className="text-2xl">{result}</span>
-            </form>
-        </div>
+            <Button type="submit" className="text-lg">
+                Send
+            </Button>
+            <span className="text-2xl">{result}</span>
+        </form>
     )
 }
